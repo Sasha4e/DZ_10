232403ffe57c37dc1914d5078529d1469e158873
Sasha Cherniavskiy <designer.citi@gmail.com>
2021-01-13 13:47:21 +0200

    lokk's like it's done

--- src/main.js
+++ src/main.js
@@ -1,72 +1,92 @@
-// Вам нужно создать список задач (объект), который умеет:
-
-// добавлять новые задачи. У каждой задачи есть статус, текст и уникальный id.
-// удалять задачу
-// редактировать задачу
-// изменять статус заметки на "выполненно"
-
-
-// Вам нужно оформить это, как объект с методами, которые нельзя удалять и редактировать + сам объект
-// не должен быть расширяем.
-
 const toDo = {
-	_toDoList: []
+	_toDoList: [],
 }
 
 Object.defineProperties(toDo, {
 
 	'toDoList': {
 		get: function() {
-			return this._toDoList;
+			return this._toDoList
 		},
 
 		set: function(value) {
-			this._toDoList = value;
+			this._toDoList = value
 		}
+	},
+
+	'getIndex': {
+		value: function(value) {
+			return this.toDoList.findIndex(note => note.task === value)
+		},
 	},
 
 	'add': {
 		value: function(taskName) {
-				toDo.toDoList.push({
-					task: taskName,
-					id: Date.now(),
-					complite: false
-				})
+			
+			if (this.toDoList.some(note => note.task === taskName))
+				throw `Error. You already had "${taskName}" note`
+
+			toDo.toDoList.push({
+				task: taskName,
+				id: Date.now(),
+				complete: false
+			})
 		}
 	},
 
 	'delete': {
-		value: function(taskName) {
-			for (let i = 0; i < toDo.toDoList.length; i++) {
-				if (toDo.toDoList[i].task === taskName) {
-					toDo.toDoList.splice(i, 1)
-				}
-			}
+		value: function(taskName, isSure = false) {
+			if (isSure)
+				this.toDoList.splice(this.getIndex(taskName), 1)
 		}
 	},
-
 
 	'edit': {
-		value: function(taskName, newTaskName) {
-			for (let i = 0; i < toDo.toDoList.length; i++) {
-				if (toDo.toDoList[i].task === taskName) {
-					toDo.toDoList[i].task = newTaskName
-				}
-			}
+		value: function(taskName, newTaskName, isSure = false) {
+			
+				if (this.toDoList.some(note => note.task === newTaskName))
+					throw `Error. You already had "${newTaskName}" note`
+				if (isSure) 
+					this.toDoList[this.getIndex(taskName)].task = newTaskName
+			
 		}
 	},
-
 
 	'done': {
 		value: function(taskName) {
-			for (let i = 0; i < toDo.toDoList.length; i++) {
-				if (toDo.toDoList[i].task === taskName) {
-					toDo.toDoList[i].complite = !toDo.toDoList[i].complite
-				}
-			}
+			this.toDoList[this.getIndex(taskName)].complete = !this.toDoList[this.getIndex(taskName)].complete
 		}
-	}
+	},
+
+	'status': {
+		value: function() {
+			completeCount = 0;
+			this.toDoList.forEach((value) => {
+				if (value.complete) completeCount++
+			})
+
+			return (
+				`
+					Всего задач: ${this.toDoList.length},
+					Выполнено задач: ${completeCount}
+					Осталось выполнить задач: ${this.toDoList.length - completeCount}
+				`
+			)
+		}
+}
 
 });
 
 Object.freeze(toDo);
+
+toDo.add("hello world");
+toDo.add("hello world1");
+toDo.add("hello world2");
+toDo.add("hello world3");
+toDo.add("hello world4");
+toDo.delete("hello world2", confirm(`are you sure you want to delete this note?` ));
+toDo.edit("hello world", "hello world5", confirm(`are you sure you want to edit this note?`));
+toDo.done("hello world3");
+
+console.log(toDo.toDoList);
+console.log(toDo.status());
